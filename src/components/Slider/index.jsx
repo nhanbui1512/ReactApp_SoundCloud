a039a9243b609eb7cd194ed53ac8a0549f47d5e8
nhanbui1512 <buithiennhan0345@gmail.com>
2024-01-23 22:06:08 +0700

    add prettier

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,78 +1,73 @@
-import classNames from "classnames/bind";
-import styles from "./Slider.module.scss";
-import Gallery from "components/Gallery";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronLeft,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { useRef, useState } from "react";
+import classNames from 'classnames/bind';
+import styles from './Slider.module.scss';
+import Gallery from 'components/Gallery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useRef, useState } from 'react';
 const cx = classNames.bind(styles);
 
 const galleries = [
   {
-    name: "Ai Đưa Em Về - 1nG x VoVanDuc (Official MV - link in Description )",
-    track: "Related track",
+    name: 'Ai Đưa Em Về - 1nG x VoVanDuc (Official MV - link in Description )',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-9b6a3bc2-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBNT1JFX09GX1dIQVRfWU9VX0xJS0UKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjYwMTM4NDM2MiIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjUxMjI0ODczNCIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjY2OTMyNzI1MSIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjczNTM1MTA5NyIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjQzNzg4OTc5OCIK",
+      'https://al.sndcdn.com/labs-9b6a3bc2-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBNT1JFX09GX1dIQVRfWU9VX0xJS0UKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjYwMTM4NDM2MiIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjUxMjI0ODczNCIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjY2OTMyNzI1MSIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjczNTM1MTA5NyIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjQzNzg4OTc5OCIK',
   },
   {
-    name: "QUERRY - QNT x TRUNG TRẦN ft RPT MCK (Prod By RASTZ)",
-    track: "Related track",
+    name: 'QUERRY - QNT x TRUNG TRẦN ft RPT MCK (Prod By RASTZ)',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-5e723669-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBNT1JFX09GX1dIQVRfWU9VX0xJS0UKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEyNzkyMDczMDMiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMTU2NzY4MzMwIgp1cm5zOiAic291bmRjbG91ZDp0cmFja3M6MTU3MjM1MDU4NCIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjE1MTY2NTIyNzIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMTg1NTQ5ODk1Igo=",
+      'https://al.sndcdn.com/labs-5e723669-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBNT1JFX09GX1dIQVRfWU9VX0xJS0UKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEyNzkyMDczMDMiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMTU2NzY4MzMwIgp1cm5zOiAic291bmRjbG91ZDp0cmFja3M6MTU3MjM1MDU4NCIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjE1MTY2NTIyNzIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMTg1NTQ5ODk1Igo=',
   },
   {
-    name: "Weekly",
-    track: "Related track",
+    name: 'Weekly',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=",
-  },
-
-  {
-    name: "pov: it is 2am and you are studying",
-    track: "Discovery Playlists",
-    image:
-      "https://i1.sndcdn.com/artworks-Zxf4PcBkqmXPzn5P-v3hjdA-t200x200.jpg",
+      'https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=',
   },
 
   {
-    name: "Focus Brainwaves",
-    track: "Discovery Playlists",
-    image:
-      "https://i1.sndcdn.com/artworks-pfo2S0KRtLgvqp9E-IGQwyw-t200x200.jpg",
+    name: 'pov: it is 2am and you are studying',
+    track: 'Discovery Playlists',
+    image: 'https://i1.sndcdn.com/artworks-Zxf4PcBkqmXPzn5P-v3hjdA-t200x200.jpg',
   },
 
   {
-    name: "Weekly",
-    track: "Related track",
-    image:
-      "https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=",
+    name: 'Focus Brainwaves',
+    track: 'Discovery Playlists',
+    image: 'https://i1.sndcdn.com/artworks-pfo2S0KRtLgvqp9E-IGQwyw-t200x200.jpg',
   },
 
   {
-    name: "Weekly",
-    track: "Related track",
+    name: 'Weekly',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=",
+      'https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=',
+  },
+
+  {
+    name: 'Weekly',
+    track: 'Related track',
+    image:
+      'https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=',
   },
   {
-    name: "Weekly",
-    track: "Related track",
+    name: 'Weekly',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=",
+      'https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=',
   },
   {
-    name: "Weekly",
-    track: "Related track",
+    name: 'Weekly',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=",
+      'https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=',
   },
   {
-    name: "Weekly",
-    track: "Related track",
+    name: 'Weekly',
+    track: 'Related track',
     image:
-      "https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=",
+      'https://al.sndcdn.com/labs-9576a784-0-t200x200.jpg?q=YXJ0d29ya190eXBlOiBTQ19XRUVLTFkKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjEwMDY0ODgzMjIiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo1MjEwMjA4MzkiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo4ODEyMTc5NDYiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczo3Nzk1Mjc2MDAiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMDg2MTQ0OTEzIgo=',
   },
 ];
 const totalPage = Math.ceil(galleries.length / 4) - 1;
@@ -86,18 +81,18 @@ function Slider() {
     distanceSlide -= (4 - residual) * 190;
   }
   return (
-    <div className={cx("wrapper")}>
+    <div className={cx('wrapper')}>
       <div
         style={{
-          width: "100%",
-          overflow: "hidden",
+          width: '100%',
+          overflow: 'hidden',
         }}
       >
         <div
           style={{
-            "--distance-slide": `-${distanceSlide}px`,
+            '--distance-slide': `-${distanceSlide}px`,
           }}
-          className={cx("list-gallery")}
+          className={cx('list-gallery')}
           ref={listRef}
         >
           {galleries.map((item, index) => (
@@ -114,9 +109,9 @@ function Slider() {
               setPage(newPage);
             }
           }}
-          className={cx("right-wraper")}
+          className={cx('right-wraper')}
         >
-          <button className={cx("slide-btn")}>
+          <button className={cx('slide-btn')}>
             <FontAwesomeIcon icon={faChevronRight} />
           </button>
         </div>
@@ -130,9 +125,9 @@ function Slider() {
               setPage(newPage);
             }
           }}
-          className={cx("left-wraper")}
+          className={cx('left-wraper')}
         >
-          <button className={cx("slide-btn")}>
+          <button className={cx('slide-btn')}>
             <FontAwesomeIcon icon={faChevronLeft} />
           </button>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,30 @@
-import classNames from "classnames/bind";
-import styles from "./Home.module.scss";
-import ListDisk from "components/ListDisk";
-import Sidebar from "./Sidebar_Right/Sidebar";
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+import ListDisk from 'components/ListDisk';
+import Sidebar from './Sidebar_Right/Sidebar';
 
 const cx = classNames.bind(styles);
 
 const Home = () => {
   return (
-    <div className={cx("wrapper")}>
+    <div className={cx('wrapper')}>
       <div
         style={{
-          position: "relative",
+          position: 'relative',
         }}
       >
-        <div className={cx("content")}>
-          <div className={cx("trending-wrapper")}>
+        <div className={cx('content')}>
+          <div className={cx('trending-wrapper')}>
             <ListDisk />
-            <ListDisk title={"Study"} />
-            <ListDisk title={"More of what you like"} />
+            <ListDisk title={'Study'} />
+            <ListDisk title={'More of what you like'} />
             <ListDisk title={`Today's Mixes`} />
-            <ListDisk title={"Trending Music on SoundCloud"} />
+            <ListDisk title={'Trending Music on SoundCloud'} />
           </div>
         </div>
-        
-        <div className={cx("side-bar")}>
-          <Sidebar/>
+
+        <div className={cx('side-bar')}>
+          <Sidebar />
         </div>
       </div>
     </div>

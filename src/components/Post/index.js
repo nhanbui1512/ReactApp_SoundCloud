597eed2bdcced7f73d9e14a3c89ef41bd717ddd3
nhanbui1512 <buithiennhan0345@gmail.com>
2024-01-25 20:48:51 +0700

    final commit 25/1/2024

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,67 +1,60 @@
-import classNames from "classnames/bind";
-import styles from "./Post.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserAlt,
-  faUserCheck,
-} from "@fortawesome/free-solid-svg-icons";
+import classNames from 'classnames/bind';
+import styles from './Post.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt, faUserCheck } from '@fortawesome/free-solid-svg-icons';
 
-import "tippy.js/dist/tippy.css";
-import "tippy.js/animations/scale-subtle.css";
-import { useEffect, useRef, useState } from "react";
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale-subtle.css';
+import { useEffect, useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Post({ data }) {
-  const [moreMenu, setMoreMenu] = useState(false);
   const moreBtnRef = useRef();
-  const [isFollowing, setIsFollowing] = useState(false)
+  const [isFollowing, setIsFollowing] = useState(false);
   useEffect(() => {
     const handleClickOutside = (event) => {
       // Kiểm tra xem sự kiện click có xảy ra ngoài nút button không
       if (moreBtnRef.current && !moreBtnRef.current.contains(event.target)) {
         // Thực hiện hành động khi click ra ngoài
-        setMoreMenu(false);
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-    
-}, []);
+  }, []);
 
-const handleFollowing = () => {
-    setIsFollowing(!isFollowing)
-}
+  const handleFollowing = () => {
+    setIsFollowing(!isFollowing);
+  };
   return (
-    <div className={cx("modul-left_item")}>
-      <div className={cx("modul-left_item-container-img")}>
-        <img className={cx("modul-left_image")} src={data.image} alt="" />        
+    <div className={cx('modul-left_item')}>
+      <div className={cx('modul-left_item-container-img')}>
+        <img className={cx('modul-left_image')} src={data.image} alt="" />
       </div>
 
-      <a href="/" className={cx("name-post")}>
+      <a href="/" className={cx('name-post')}>
         {data.name}
       </a>
-      <span className={cx("name-post")}>
+      <span className={cx('name-post')}>
         <div>
-            <FontAwesomeIcon
-                className={cx("")}
-                icon={faUserAlt}
-            />
-            <span className={cx("followers-post")}>1,728 followers</span>
+          <FontAwesomeIcon className={cx('')} icon={faUserAlt} />
+          <span className={cx('followers-post')}>1,728 followers</span>
         </div>
 
         <div className={cx('box-btn')}>
-            <div onClick={() => {handleFollowing()}} className={cx("btn", { following: isFollowing })}>
-                <FontAwesomeIcon
-                    className={cx("")}
-                    icon={faUserCheck}
-                />
-                <span className={cx("followers-post")}>Following</span>
-            </div>
+          <div
+            onClick={() => {
+              handleFollowing();
+            }}
+            className={cx('btn', { following: isFollowing })}
+          >
+            <FontAwesomeIcon className={cx('')} icon={faUserCheck} />
+            <span className={cx('followers-post')}>Following</span>
+          </div>
         </div>
       </span>
     </div>

5240ccaceecae898b37fd12b773b10e90b9305f8
huyle <lehuuduchuy@gmail.com>
2024-02-01 20:11:48 +0700

    update UI

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 const request = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://1e77-113-166-1-15.ngrok-free.app',
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+  }
 });
 
 export const get = async (path, options = {}) => {

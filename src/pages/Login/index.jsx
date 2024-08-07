ae418f191d181f9278f377ca8f7a157726c9c8bf
Nhan Bui <buithiennhan0345@gmail.com>
2024-08-07 21:40:53 +0700

    add feature visible password

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,12 +5,16 @@ import { StorageContext } from 'context/Storage';
 import { CheckLogin } from 'api/Login';
 import styles from './Login.module.scss';
 import { setToken } from 'services/local/cookie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { faEye as faEyeSollid } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [fail, setFail] = useState(false);
+  const [isHiddenPassword, setIsHiddenPassword] = useState(true);
 
   const storage = useContext(StorageContext);
 
@@ -47,14 +51,20 @@ const Login = () => {
           onChange={(e) => setEmail(e.target.value)}
         />
         <br></br>
-        <input
-          className="text-[14px]"
-          value={password}
-          type="password"
-          placeholder="Your Password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <div className="relative flex justify-center">
+          <input
+            className="text-[14px]"
+            value={password}
+            type={isHiddenPassword ? 'password' : 'text'}
+            placeholder="Your Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <span onClick={() => setIsHiddenPassword(!isHiddenPassword)} className={cx('hidden-btn')}>
+            <FontAwesomeIcon fontSize={14} icon={isHiddenPassword ? faEye : faEyeSollid} />
+          </span>
+        </div>
+
         <br></br>
 
         {fail && (

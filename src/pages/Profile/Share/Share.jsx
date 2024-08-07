ac8e0fbce4b3067d6b6a7d315313375713ad4dc2
Nhan Bui <buithiennhan0345@gmail.com>
2024-08-07 21:43:40 +0700

    fix css shareProfile popper

--- src/pages/Profile/Share/Share.jsx
+++ src/pages/Profile/Share/Share.jsx
@@ -1,18 +1,17 @@
 import classNames from 'classnames/bind';
-import { useRef, useState, useEffect} from 'react';
+import { useRef, useState, useEffect } from 'react';
 import styles from './Share.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import {twitter,facebook,tumblr,pinterest,email} from './icon'
+import { twitter, facebook, tumblr, pinterest, email } from './icon';
 const cx = classNames.bind(styles);
 
 function Share({ setPopperShare = {} }) {
-
-  const [shareLink, setShareLink] = useState(''); 
+  const [shareLink, setShareLink] = useState('');
   const inputRef = useRef(null);
 
   useEffect(() => {
-    setShareLink(window.location.href );
+    setShareLink(window.location.href);
   }, []);
 
   const handleCopyLink = () => {
@@ -30,7 +29,7 @@ function Share({ setPopperShare = {} }) {
     //   document.execCommand('copy');
     // }
   };
- 
+
   return (
     <div className={cx('wrapper')}>
       <div className={cx('main-content')}>
@@ -44,20 +43,28 @@ function Share({ setPopperShare = {} }) {
             <FontAwesomeIcon icon={faXmark} />
           </button>
 
-          <h2 >Share link</h2>
+          <h2>Share link</h2>
           <div className={cx('ShareLink')}>
-              <div className={cx('internet-social')}>
-              <img src={twitter} alt="Edit Icon" className={cx('social-icon')}/>
-              <img src={facebook} alt="Edit Icon" className={cx('social-icon')}/>
-              <img src={tumblr} alt="Edit Icon" className={cx('social-icon')}/>
-              <img src={pinterest} alt="Edit Icon" className={cx('social-icon')}/>
-              <img src={email} alt="Edit Icon" className={cx('social-icon')}/>
-              </div>
-              <div className={cx('copylink')}>
-                <input type="text" ref={inputRef} value={shareLink} readOnly /> 
-                <button onClick={handleCopyLink}>Copy link</button>
-              </div>
-          </div> 
+            <div className={cx('flex mb-2')}>
+              <img src={twitter} alt="Edit Icon" className={cx('social-icon')} />
+              <img src={facebook} alt="Edit Icon" className={cx('social-icon')} />
+              <img src={tumblr} alt="Edit Icon" className={cx('social-icon')} />
+              <img src={pinterest} alt="Edit Icon" className={cx('social-icon')} />
+              <img src={email} alt="Edit Icon" className={cx('social-icon')} />
+            </div>
+            <div className={cx('copylink')}>
+              <input
+                className="text-[14px]"
+                type="text"
+                ref={inputRef}
+                value={shareLink}
+                readOnly
+              />
+              <button className="text-[14px]" onClick={handleCopyLink}>
+                Copy link
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>

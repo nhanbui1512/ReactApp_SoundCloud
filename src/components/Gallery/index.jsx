cbb26dc747e670fc6d4931d3ecf07b61d887fb0a
nhanbui1512 <buithiennhan0345@gmail.com>
2024-09-09 19:55:07 +0700

    fix bug music player

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -171,7 +171,7 @@ function Gallery({ data, playLists }) {
         const newState = [...prev];
         // đảm bảo bài muốn thêm vào sau không phải là bài đang phát
         // 2 trường hợp : không được phát nhưng tồn tại trong pl , không được phát và cũng không tồn tại trong pl
-        if (storage.currentPlayList[indexPlaying].id !== data.id) {
+        if (storage.currentPlayList[indexPlaying]?.id !== data.id) {
           if (indexOfSongInPlaylist !== -1) {
             // Xóa phần tử khỏi vị trí cũ
             newState.splice(indexOfSongInPlaylist, 1);

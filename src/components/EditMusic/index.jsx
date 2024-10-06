e7fc75c03c9e8842220d5b8024508d0f5dbbe127
Nhan Bui <buithiennhan0345@gmail.com>
2024-10-06 16:03:40 +0700

    update Edit Music popup UI

--- src/components/EditMusic/index.jsx
+++ src/components/EditMusic/index.jsx
@@ -1,4 +1,4 @@
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faCircleExclamation, faDownload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from 'components/Button';
 import React from 'react';
@@ -36,7 +36,58 @@ function EditMusic(props) {
               alt=""
             />
           </div>
-          <div className="flex-1">Right</div>
+          <div className="flex-1">
+            <div className="flex flex-col mb-[10px]">
+              <span className={cx('label')}>Title*</span>
+              <input className="border-[#ccc] border-solid rounded-[4px] px-[7px] py-1 text-[14px] border-[1px]" />
+            </div>
+            <div className="flex flex-col mb-[10px]">
+              <span className={cx('label')}>Permalink*</span>
+              <span className="text-[14px] text-[#999]">soundcloud.com/nhanbui1512/</span>
+            </div>
+
+            <div className="flex flex-col mb-[10px]">
+              <span className={cx('label')}>Genre*</span>
+              <span className="text-[14px] text-[#999]">soundcloud.com/nhanbui1512/</span>
+            </div>
+
+            <div className="flex flex-col mb-[10px]">
+              <span className={cx('label')}>Description</span>
+              <textarea
+                rows={5}
+                className="border-[#ccc] border-solid rounded-[4px] px-[7px] py-1 text-[14px] border-[1px]"
+              />
+            </div>
+
+            <div className="flex flex-col mb-[10px]">
+              <div className="text-[#999] text-[12px]">
+                <FontAwesomeIcon icon={faCircleExclamation} />
+                <span className="ml-2">Editing the caption doesn’t post the track again.</span>
+              </div>
+              <span className={cx('label')}>Privacy:</span>
+              <div>
+                <label className="text-[12px] text-[#333] flex items-start gap-1 font-bold">
+                  <input type="radio" value="public" name="radio4673" />
+                  <div className=""></div>
+                  <span className="radioGroup__label">
+                    <span className="radioGroup__labelText sc-text-h4" data-public="">
+                      Public
+                    </span>
+                  </span>
+                </label>
+
+                <label className="text-[12px] text-[#333] flex items-start gap-1 font-bold">
+                  <input type="radio" value="public" name="radio4673" />
+                  <div className=""></div>
+                  <span className="radioGroup__label">
+                    <span className="radioGroup__labelText sc-text-h4" data-public="">
+                      Private
+                    </span>
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
